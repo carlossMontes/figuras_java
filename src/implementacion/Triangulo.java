72bad9e594dbd462cb8d990cb4cc0c0521d02ec4
chaozwoah <carloss.montes@gmail.com>
2021-05-07 00:38:09 -0500

    no pude

--- src/implementacion/Triangulo.java
+++ src/implementacion/Triangulo.java
@@ -7,46 +7,59 @@ public class Triangulo {
 		String caract = "* ";
 		String espacio = " ";
 		
-		// Triangulo rectangulo hacia abajo
+//		// Triangulo rectangulo hacia abajo
+//		for (int i = 1; i <= cant; i++) {
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print(caract);
+//			}
+//			System.out.println();
+//		}
+		
+//		System.out.println();
+		
+		// al reves
 		for (int i = 1; i <= cant; i++) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print(caract);
+				System.out.print(espacio + j);
+				for (int k = cant; k >= j; k--) {
+					System.out.print(caract);
+				}
 			}
 			System.out.println();
 		}
 		
-		System.out.println();
-		
-		// Triangulo rectangulo hacia arriba
-		for (int i = cant; i >= 1; i--) {
-			for (int j = i; j >= 1; j--) {
-				System.out.print(caract);	
-			}
-			System.out.println();
-		}
-		
-		// Triangulo normal
-		for (int i = cant; i >= 0; i--) {
-			for (int j = i; j >= 0; j--) {
-				System.out.print(espacio);	
-			}
-			for (int j = i; j < cant; j++) {
-				System.out.print(caract);
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		// Impresion de cabeza
-		for (int i = 1; i <= cant; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print(espacio);
-			}
-			for (int j = i; j <= cant; j++) {
-				System.out.print(caract);
-			}
-			System.out.println();
-		}
+//		System.out.println();
+//		
+//		// Triangulo rectangulo hacia arriba
+//		for (int i = cant; i >= 1; i--) {
+//			for (int j = i; j >= 1; j--) {
+//				System.out.print(caract);	
+//			}
+//			System.out.println();
+//		}
+//		
+//		// Triangulo normal
+//		for (int i = cant; i >= 0; i--) {
+//			for (int j = i; j >= 0; j--) {
+//				System.out.print(espacio);	
+//			}
+//			for (int j = i; j < cant; j++) {
+//				System.out.print(caract);
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
+//		
+//		// Impresion de cabeza
+//		for (int i = 1; i <= cant; i++) {
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print(espacio);
+//			}
+//			for (int j = i; j <= cant; j++) {
+//				System.out.print(caract);
+//			}
+//			System.out.println();
+//		}
 	}
 }
